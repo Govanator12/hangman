4ea3f6fce4861c5d30057fa53719877b9e1a9c96
Steven Hardy <steverhardy@gmail.com>
2019-04-19 11:54:49 -0400

    Removed word generator and added own wordlists

--- hangman.py
+++ hangman.py
@@ -1,47 +1,48 @@
-from random_word import RandomWords
-r = RandomWords()
+import random
 
 class Hangman():
 
-    def __init__():
+    def __init__(self):
         pass
 
     # play function that actually handles the game
 
-    def play():
-
+    def play(self):
         # defining some variables to be used later
         guesses = []
         guesses_remaining = 7
 
+        colors = ['red', 'blue', 'green', 'beige', 'teal', 'black', 'orange', 'purple', 'gold', 'brown', 'yellow']
+
+        animals = ['dog', 'cat', 'bear', 'hawk', 'lion', 'cow', 'horse', 'fish', 'pig', 'alligator']
+
+        video_games = ['halo','starcraft', 'tekken', 'squad', 'skate', 'warcraft', 'spore', 'killzone', 'spyro', 'mario']
+
         while True:
             print('Please select a category using a number:')
-            print('1. Verbs')
-            print('2. Nouns')
-            print('3. Adjectives')
-            print('4. Random')
+            print('1. Colors')
+            print('2. Animals')
+            print('3. Video games')
 
             category = int(input())
             word = ""
 
             if category == 1:
-                word = r.get_random_word(includePartOfSpeech='verb')
+                word = random.choice(colors)
                 break
 
             elif category == 2:
-                word = r.get_random_word(includePartOfSpeech='noun')
+                word = random.choice(animals)
                 break
 
             elif category == 3:
-                word = r.get_random_word(includePartOfSpeech='adjective')
-                break
-
-            elif category == 4:
-                word = r.get_random_word()
+                word = random.choice(video_games)
                 break
 
             else:
                 print('Please select an option from the menu by entering a number')
+
+        characters = list(word)
 
         while True:
             for letter in word:
@@ -49,23 +50,34 @@ class Hangman():
                     print(f'{letter} ', end ='')
                 else:
                     print('_ ', end = '')
-            print(f'\nYou have {guesses_remaining} remaining')
+
+                if set(characters).issubset(guesses):
+                    print('Nice work you win!')
+                    break
+
+            print(f'\nYou have {guesses_remaining} guesses remaining')
             print(f'Your guesses so far: {guesses}')
 
             if guesses_remaining > 0:
-                current_guess = input(f'You have {guesses_remaining} guesses left. Guess again!: ')
+                current_guess = input(f'Guess again!: ')
 
-                if (len(current_guess) > 1) or !current_guess.isalpha():
+                if (len(current_guess) > 1) or current_guess.isalpha() == False:
                     print('Please only guess single letters')
                     continue
 
-                elif current_guess in guesses:
+                elif current_guess.lower() in guesses:
                     print(f"You've already guessed '{current_guess}'!")
                     continue
 
                 else:
-                    guesses.append(current_guess)
-                    guesses_remaining -= 1
+                    guesses.append(current_guess.lower())
+
+                    if current_guess in word:
+                        print('Good guess! \n\n')
+
+                    else:
+                        print('No dice :( \n\n')
+                        guesses_remaining -= 1
 
             else:
                 print('Out of guesses! Better luck next time!')
@@ -73,14 +85,14 @@ class Hangman():
 
     # menu function that starts the game
 
-    def menu():
+    def menu(self):
         while True:
             print("\n\nWELCOME TO HANGMAN")
             print("-------------------")
             ans = input('Play a game? (Y/N): ')
 
             if ans.lower() == 'y':
-                play()
+                Hangman.play(self)
 
             elif ans.lower() == 'n':
                 print('Thanks for playing!')
@@ -88,3 +100,7 @@ class Hangman():
 
             else:
                 print('Please enter "Y" or "N"')
+
+
+my_hangman = Hangman()
+my_hangman.menu()
